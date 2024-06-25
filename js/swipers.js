b6a04f7fdc56aa75189b593ce6761f27ece4094f
Daniil <baldindaniilza@mail.ru>
2024-06-25 18:50:52 +0500

    Компонент слайдера

--- js/swipers.js
+++ js/swipers.js
@@ -1,40 +1,22 @@
 import Swiper from "swiper";
 import "swiper/css";
 
-const images = [
-	"/assets/img/products/product1.png",
-	"/assets/img/background/bg-about.png",
-	"/assets/img/background/bg-employees.png",
-	"/assets/img/background/bg-main.png",
-	"/assets/img/projects/project1.png",
-];
-
-const createSlide = (image) => {
-	return /*html*/ `
-        <button class="product-card__img-button swiper-slide">
-            <img
-                src="${image}"
-                class="product-card__img product-card__img--small"
-            />
-        </button>
-    `;
-};
-
 const syncSwipers = (swiper1, swiper2, swiperNumber) => {
 	if (swiperNumber === 1) {
 		swiper2.activeIndex = swiper1.activeIndex;
 		swiper2.slideTo(swiper1.activeIndex);
 	}
-    if (swiperNumber === 2) {
-        swiper1.activeIndex = swiper2.clickedIndex;
-        swiper1.slideTo(swiper2.clickedIndex);
-    }
+	if (swiperNumber === 2) {
+		swiper1.activeIndex = swiper2.clickedIndex;
+		swiper1.slideTo(swiper2.clickedIndex);
+	}
 };
 
 const productSwiperPrevButton = document.getElementById("productSwiperPrev");
 const productSwiperNextButton = document.getElementById("productSwiperNext");
 const productSwiper = new Swiper(".productSwiper", {
 	autoplay: true,
+    spaceBetween: 20,
 	navigation: {
 		nextEl: "#productSwiperNext",
 		prevEl: "#productSwiperPrev",
@@ -43,6 +25,7 @@ const productSwiper = new Swiper(".productSwiper", {
 const productSwiper2 = new Swiper(".productSwiper2", {
 	slidesPerView: 4,
 	slideToClickedSlide: true,
+    spaceBetween: 20,
 });
 
 productSwiper.on("slideChange", () => syncSwipers(productSwiper, productSwiper2, 1));
@@ -60,11 +43,20 @@ const aboutSwiperPrevButton = document.getElementById("aboutSwiperPrev");
 const aboutSwiperNextButton = document.getElementById("aboutSwiperNext");
 const aboutSwiper = new Swiper(".aboutSwiper", {
 	autoplay: true,
+    spaceBetween: 20,
 	navigation: {
 		nextEl: "#aboutSwiperNext",
 		prevEl: "#aboutSwiperPrev",
 	},
 });
+const aboutSwiper2 = new Swiper(".aboutSwiper2", {
+	slidesPerView: 4,
+	slideToClickedSlide: true,
+    spaceBetween: 20,
+});
+
+aboutSwiper.on("slideChange", () => syncSwipers(aboutSwiper, aboutSwiper2, 1));
+aboutSwiper2.on("click", () => syncSwipers(aboutSwiper, aboutSwiper2, 2));
 
 aboutSwiperNextButton.addEventListener("click", () => {
 	aboutSwiper.slideNext();
